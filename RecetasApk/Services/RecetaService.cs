4c575a4e51c4c7abd30c777858168e0d8f658186
MatiasMadoery <matimadoery@hotmail.com>
2025-03-31 09:29:50 -0300

    Edit function added

--- RecetasApk/Services/RecetaService.cs
+++ RecetasApk/Services/RecetaService.cs
@@ -7,14 +7,14 @@ namespace RecetasApk.Services
     {
         private readonly RecetaDatabase? _database;
 
-        public RecetaService(RecetaDatabase? database)
+        public RecetaService(RecetaDatabase database)
         {
-            _database = database;
+            _database = database ?? throw new ArgumentNullException(nameof(database));
         }
 
         public Task<List<Receta>> GetRecetasAsync() => _database!.GetRecetasAsync();
         public Task<Receta> GetRecetaAsync(int id) => _database!.GetRecetaAsync(id);
         public Task<int> SaveRecetaAsync(Receta receta) => _database!.SaveRecetaAsync(receta);
-        public Task<int> DeleteRecetaAsync(Receta receta) => _database!.DeleteRecetaAsync(receta);
+        public Task<int> DeleteRecetaAsync(Receta receta) => _database!.DeleteRecetaAsync(receta);        
     }
 }
